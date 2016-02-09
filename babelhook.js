9719202cf12cee4aaea8ad1853683a3fd23aff5d
Tomasz Wesołowski <kosashi@gmail.com>
2016-02-09 23:02:11 +0100

    Set up jsdom for tests
    Meh

--- babelhook.js
+++ babelhook.js
@@ -1,3 +1,9 @@
 require("babel-core/register")({
   "presets": ["es2015", "react"]
 });
+
+var jsdom = require('jsdom').jsdom;
+
+global.document = jsdom('<!doctype html><html><body></body></html>');
+global.window = document.defaultView;
+global.navigator = global.window.navigator;
--- test/utils.js
+++ test/utils.js
@@ -1,4 +1,6 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
+import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-addons-test-utils';
 import expect from 'expect';
 import expectJSX from 'expect-jsx';
 import * as UrlUtils from '../src/UrlUtils.js';
@@ -17,5 +19,17 @@ describe("foo", function() {
 
   it("should react", function() {
     expect(<div><span>123</span></div>).toIncludeJSX(<span>123</span>);
+  });
+
+  it("should render with jsdom", function() {
+    let foo = ReactTestUtils.renderIntoDocument(
+      <h1 className="hi"><span>Hello there</span></h1>
+    );
+    expect(foo).toExist();
+    let node = ReactDOM.findDOMNode(foo);
+    expect(node.classList[0]).toEqual("hi");
+    expect(node.childNodes[0].tagName).toEqual("SPAN");
+    expect(node.textContent).toEqual("Hello there");
+
   });
 });
